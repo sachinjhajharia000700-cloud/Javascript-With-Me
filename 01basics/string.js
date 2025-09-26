667afa0ee99e6cab253f20b795bcaaabb34cf963
sachin <sachinjhajharia000700@gmail.com>
2025-09-26 08:19:34 +0000

    string

---
+++ 01basics/string.js
@@ -0,0 +1,10 @@
+let a = "saka";
+let b = "kaka";
+let c = a+b;
+console.log(c);  // ye to normal example hai and ye to saab janta bhi hai ki 2 string  ko add karne per  dono merge ho jati hai 
+
+
+
+let d = "raka";
+let e = "laka";
+console.log(`firstname${raka} + second name${laka}`);
